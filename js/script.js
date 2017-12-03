996acff0967e9f784b9dc954dac20746dc7493c4
Nemolite <g25092011@mail.ru>
2017-12-03 09:19:47 +0300

    correction

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    
+  });
+
+$(document).ready(function(){
+    
+    $("#id-menu").click(function(){$("#glob-menu").fadeIn(1500)});
+    $("#glob-menu").click(function(){$("#glob-menu").fadeOut(1500)});
+  });
